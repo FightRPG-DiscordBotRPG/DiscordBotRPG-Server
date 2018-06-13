a6e0aaeb53c78759191a7cb002ee6f13fbb33386
Jean-Charles TECHER <techer.jeancharles974@gmail.com>
2018-06-13 17:12:40 +0400

    * Correciton erreur de typo

--- sql/Donjons&boss/initial.sql
+++ sql/Donjons&boss/initial.sql
@@ -1,5 +1,5 @@
 INSERT INTO `monstrestypes` (`idType`, `nom`) VALUES (NULL, 'boss');
-INSERT INTO `areas` (`idArea`, `AreaName`, `AreaDesc`, `AreaImage`, `AreaLevels`, `idAreaType`) VALUES (NULL, 'Les nappes feu-à-tiques', `Il fait chaud non ? Oui il faut chaud.`, 'https://pbs.twimg.com/media/CmOgb8vWEAARI_g.jpg', '20', '3');
+INSERT INTO `areas` (`idArea`, `AreaName`, `AreaDesc`, `AreaImage`, `AreaLevels`, `idAreaType`) VALUES (NULL, 'Les nappes feu-à-tiques', 'Il fait chaud non ? Oui il faut chaud.', 'https://pbs.twimg.com/media/CmOgb8vWEAARI_g.jpg', '20', '3');
 INSERT INTO `monstres` (`idMonstre`, `name`, `avglevel`, `idType`) VALUES (NULL, 'Bats le Rog, esprit du feu', '20', '3'), (NULL, 'Fouettard ardent', '20', '2');
 
 -- Bats le rog ici
