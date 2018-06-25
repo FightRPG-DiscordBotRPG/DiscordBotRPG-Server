5e9b2b1dee34e97127477e94f4676c305c5e09b5
Jean-Charles TECHER <techer.jeancharles974@gmail.com>
2018-06-25 19:19:11 +0400

    + Ajotu du batiment de craft et de l'objet craft représentant un craft

---
+++ bin/CraftSystem/Craft.js
@@ -0,0 +1,84 @@
+const conn = require("../../conf/mysql");
+const Globals = require("../Globals");
+const Translator = require("../Translator/Translator");
+const Discord = require("discord.js");
+
+class Craft {
+    constructor(id) {
+        this.exist = false;
+        this.requiredItems = [];
+        this.itemInfo = {};
+        this.load(id);
+    }
+
+    load(id) {
+        let res = conn.query(`SELECT DISTINCT nomItem, itemsbase.idRarity, descItem, imageItem, nomType, nomRarity, couleurRarity, nomSousType, maxLevel, minLevel FROM craftitemsneeded 
+        INNER JOIN itemsbase ON itemsbase.idBaseItem = craftitemsneeded.IdCraftItem
+        INNER JOIN itemstypes ON itemsbase.idType = itemstypes.idType 
+        INNER JOIN itemsrarities ON itemsbase.idRarity = itemsrarities.idRarity INNER JOIN itemssoustypes ON itemssoustypes.idSousType = itemsbase.idSousType
+        INNER JOIN craftitem ON craftitem.idBaseItem = craftitemsneeded.IdCraftItem
+        WHERE craftitemsneeded.IdCraftItem = ?;
+        `, [id]);
+        if(res[0]) {
+            res = res[0];
+            this.exist = true;
+            this.itemInfo = {
+                name: res.nomItem,
+                desc: res.descItem,
+                image: res.imageItem,
+                typename: res.nomType,
+                stypename: res.nomSousType,
+                rarity: res.nomRarity,
+                idRarity: res.idRarity,
+                rarityColor: res.rarityColor,
+                maxLevel: res.maxLevel,
+                minLevel: res.minLevel,
+            }
+
+            res = conn.query(`SELECT nomItem, descItem, imageItem, nomType, nomRarity, couleurRarity, nomSousType, maxLevel, minLevel, number FROM craftitemsneeded 
+            INNER JOIN itemsbase ON itemsbase.idBaseItem = craftitemsneeded.NeededItem
+            INNER JOIN itemstypes ON itemsbase.idType = itemstypes.idType 
+            INNER JOIN itemsrarities ON itemsbase.idRarity = itemsrarities.idRarity INNER JOIN itemssoustypes ON itemssoustypes.idSousType = itemsbase.idSousType
+            INNER JOIN craftitem ON craftitem.idBaseItem = craftitemsneeded.IdCraftItem
+            WHERE craftitemsneeded.IdCraftItem = ?;`, [id]);
+
+            for(let item of res) {
+                this.requiredItems.push({
+                    name: item.nomItem,
+                    typename: item.nomType,
+                    stypename: item.nomSousType,
+                    rarity: item.nomRarity,
+                    number: item.number
+                });
+            }
+        }
+    }
+
+    toEmbed(lang) {
+        let embed = new Discord.RichEmbed()
+                .setAuthor(this.itemInfo.name, Globals.addr + "images/items/" + this.itemInfo.image + ".png")
+                .setColor(this.itemInfo.rarityColor)
+                .addField(Translator.getString(lang, "item_types", this.itemInfo.typename) + " (" + Translator.getString(lang, "item_sous_types", this.itemInfo.stypename) + ")" + " | " + Translator.getString(lang, "rarities", this.itemInfo.rarity) + " | " + Translator.getString(lang, "general", "lvl") + " : " + this.itemInfo.minLevel + "-" + this.itemInfo.maxLevel + " | "
+                , this.itemInfo.desc != "" ? this.itemInfo.desc : Translator.getString(lang, "inventory_equipment", "no_desc"))
+                .addField(Translator.getString(lang, "craft", "needed_items"), this.requiredItemsToStr(lang));
+        return embed;
+    }
+
+    requiredItemsToStr(lang) {
+        let str = "```\n" + Translator.getString(lang, "craft", "header") + "\n";
+
+        for(let item of this.requiredItems) {
+            str += "\n";
+            str += item.name + " - " + Translator.getString(lang, "item_types", item.typename) + " - " + Translator.getString(lang, "item_sous_types", item.stypename) + " - " + Translator.getString(lang, "rarities", item.rarity) + " - x" + item.number;
+        }
+
+        str += "```";
+        return str;
+    }
+
+    canBeCraft(buildingRarityLevel) {
+        return this.itemInfo.idRarity <= buildingRarityLevel;
+    }
+}
+
+module.exports = Craft;
---
+++ bin/CraftSystem/CraftingBuilding.js
@@ -0,0 +1,79 @@
+const conn = require("../../conf/mysql");
+const Globals = require("../Globals");
+const Translator = require("../Translator/Translator");
+const Craft = require("./Craft");
+
+class CraftingBuilding {
+    constructor() {
+        this.id = 0; 
+        this.maxRarity = 1;
+        this.maxRarityName = "";
+        this.maxRarityColor = "";
+        this.isActive = false;
+    }
+
+    load(idArea) {
+        let res = conn.query("SELECT idCraftBuilding, rarityMax, nomRarity, couleurRarity, active FROM craftbuilding INNER JOIN itemsrarities ON itemsrarities.idRarity = craftbuilding.rarityMax WHERE idArea = ?", [idArea]);
+        if (res[0]) {
+            this.id = res[0]["idCraftBuilding"];
+            this.maxRarity = res[0]["rarityMax"];
+            this.maxRarityName = res[0]["nomRarity"];
+            this.maxRarityColor = res[0]["couleurRarity"];
+            this.isActive = res[0]["active"];
+        }
+    }
+
+    getCraftingList(page) {
+        page = page ? (page <= 0 || !Number.isInteger(page) ? 1 : page) : 1;
+        let perPage = 10;
+        let maxPage = Math.ceil(conn.query(`SELECT COUNT(*) FROM craftitem
+                                INNER JOIN itemsbase ON itemsbase.idBaseItem = craftitem.idBaseItem
+                                WHERE itemsbase.idRarity <= ? ORDER BY craftitem.minLevel DESC`,
+            [this.maxRarity])[0]["COUNT(*)"] / perPage);
+        page = maxPage > 0 && maxPage < page ? maxPage : page;
+
+        let res = conn.query(`SELECT * FROM craftitem
+                    INNER JOIN itemsbase ON itemsbase.idBaseItem = craftitem.idBaseItem
+                    WHERE itemsbase.idRarity <= ? ORDER BY craftitem.minLevel DESC LIMIT ? OFFSET ?`,
+            [this.maxRarity, perPage, page - 1]);
+
+
+
+        return { res: res, maxPage: maxPage, page: page };
+    }
+
+    craftingListToEmbed(page, lang) {
+        let res = this.getCraftingList(page);
+        let str = "```\n";
+        str += Translator.getString(lang, "crafting", "header_str") + "\n\n";
+        let crafts = res.res;
+        if (crafts.length > 0) {
+            for (let craft of crafts) {
+                str += craft.idBaseItem + " - " + craft.nomItem + " - " + craft.minLevel + " - " + craft.maxLevel + " - " + Translator.getString(lang, "rarities", Globals.itemsrarities[craft.idRarity]) + "\n";
+            }
+            str += "\n";
+        } else {
+            str += Translator.getString(lang, "general", "nothing_at_this_page") + "\n\n";
+        }
+        str += Translator.getString(lang, "general", "page_out_of_x", [res.page, res.maxPage > 0 ? res.maxPage : 1])
+        str += "```";
+        return str;
+    }
+
+    craftToEmbed(idCraft, lang) {
+        idCraft = idCraft && Number.isInteger(Number.parseInt(idCraft)) ? idCraft : 0;
+
+        // Like this we dont have to call databse if id is not a number or whatever
+        if(idCraft > 0) {
+            let craft = new Craft(idCraft);
+            if(craft.exist && craft.canBeCraft(this.maxRarity)) {
+                return craft.toEmbed(lang);
+            } 
+        }
+
+        return Translator.getString(lang, "errors", "craft_dont_exist");
+    }
+
+}
+
+module.exports = CraftingBuilding;
