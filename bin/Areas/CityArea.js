158d864fbd99ae2d5d1fdf655456cdae1c762445
Jean-Charles TECHER <techer.jeancharles974@gmail.com>
2018-06-25 19:19:47 +0400

    + Ajout du nouveau service (craft) - Suppression du placehodler pnj

--- bin/Areas/CityArea.js
+++ bin/Areas/CityArea.js
@@ -4,16 +4,19 @@ const Area = require("./Area");
 const Discord = require("discord.js");
 const Translator = require("../Translator/Translator");
 const Marketplace = require("../Marketplace/Marketplace");
+const CraftingBuilding = require("../CraftSystem/CraftingBuilding");
 
 class CityArea extends Area {
 
     constructor(id) {
         super(id, id);
         this.services = {
-            "marketplace": new Marketplace()
+            "marketplace": new Marketplace(),
+            "craftingbuilding" : new CraftingBuilding()
         }
 
         this.services.marketplace.loadMakerplace(this.id);
+        this.services.craftingbuilding.load(this.id);
     }
 
     toStr(lang) {
@@ -21,7 +24,6 @@ class CityArea extends Area {
             .setColor([0, 255, 0])
             .setAuthor(this.name + " | " + this.levels + " | " + Translator.getString(lang, "area", "owned_by") + " : " + this.getOwner(lang), this.image)
             .addField(Translator.getString(lang, "general", "description"), (this.desc ? this.desc : Translator.getString(lang, "area", "no_description")) + "\n\nAvancement de la ville : **" + 1 + "**")
-            .addField("PNJ", "``` PLACEHOLDER ```")
             .addField("Services", "```- Tavernier\n- Banque\n- Marché\n- Forge```")
             .setImage(this.image);
     }
