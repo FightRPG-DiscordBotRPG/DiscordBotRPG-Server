480f7813fe72348f9c4a087e2cb04c0a9bccdc83
Jean-Charles TECHER <techer.jeancharles974@gmail.com>
2018-06-28 00:09:38 +0400

    * Modification de l'xp qui était trop grande

--- bin/CraftSystem/CraftSystem.js
+++ bin/CraftSystem/CraftSystem.js
@@ -4,13 +4,13 @@ class CraftSystem {
 
     static getXP(craftLevel, playerCraftlevel, rarity, collect) {
 
-        let xp = Math.ceil(10* (Math.pow(craftLevel, 2)) * (.5 + rarity / 2));
+        let xp = Math.ceil(10* (Math.pow(craftLevel, 2) / 1.5) * (.5 + rarity / 2));
 
         let diff = playerCraftlevel - craftLevel;
         diff = diff < 0 ? -diff : diff;
 
         if(collect) {
-            xp = xp / 5;
+            xp = xp / 10;
             xp = Math.ceil(xp * (diff > 4 ? 0.35 : 1));
         } else {
             xp = Math.ceil(xp * (diff > 2 ? 1/diff : 1));
