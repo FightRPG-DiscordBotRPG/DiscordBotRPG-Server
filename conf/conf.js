8f26ad0e0fd6aa24ce615047ec0628c712be34ce
Jean-Charles TECHER <techer.jeancharles974@gmail.com>
2018-07-01 00:48:25 +0400

    .. dev env

--- conf/conf.js
+++ conf/conf.js
@@ -1,5 +1,5 @@
 const conf = {
-    env: "prod",
+    env: "dev",
     discordbotskey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQwMTQyMTY0NDk2ODYyNDEyOSIsImJvdCI6dHJ1ZSwiaWF0IjoxNTMwMjc0MDIyfQ.PIlRw8c0L_cCWkcS7JM3XJNpv6Vj9RymEvbsvIysTFI"
 }
 
