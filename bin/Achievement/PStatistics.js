b05033c443e5c9963c3d994d1577219f28c95c84
Jean-Charles TECHER <techer.jeancharles974@gmail.com>
2018-07-26 20:45:53 +0400

    * For debug

--- bin/Achievement/PStatistics.js
+++ bin/Achievement/PStatistics.js
@@ -3,6 +3,7 @@ const conn = require("../../conf/mysql");
 
 class PStatistics extends Statistics {
     static async incrStat(idCharacter, statName, number) {
+        //console.log(idCharacter + " -> " + statName + " add " + number);
         conn.query("UPDATE charactersstatistics INNER JOIN statisticsbases ON statisticsbases.idStatisticBase = charactersstatistics.idStatisticBase SET value = value + ? WHERE idCharacter = ? AND statisticsbases.name = ?;", [number, idCharacter, statName]);
     }
 
