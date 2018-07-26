dd458224bbb0643b37f45fe810041bc4d0b0b189
Jean-Charles TECHER <techer.jeancharles974@gmail.com>
2018-07-26 16:58:04 +0400

    + Ajout des statistiques (ex: commandes tappées etc)

---
+++ bin/Achievement/PStatistics.js
@@ -0,0 +1,20 @@
+const Statistics = require("./Statistics");
+const conn = require("../../conf/mysql");
+
+class PStatistics extends Statistics {
+    static async incrStat(idCharacter, statName, number) {
+        conn.query("UPDATE charactersstatistics INNER JOIN statisticsbases ON statisticsbases.idStatisticBase = charactersstatistics.idStatisticBase SET value = value + ? WHERE idCharacter = ? AND statisticsbases.name = ?;", [number, idCharacter, statName]);
+    }
+
+    static async decrStat(idCharacter, statName, number) {
+        conn.query("UPDATE charactersstatistics INNER JOIN statisticsbases ON statisticsbases.idStatisticBase = charactersstatistics.idStatisticBase SET value = value - ? WHERE idCharacter = ? AND statisticsbases.name = ?;", [number, idCharacter, statName]);
+    }
+
+    static async setStat(idCharacter, statName, number) {
+        conn.query("UPDATE charactersstatistics INNER JOIN statisticsbases ON statisticsbases.idStatisticBase = charactersstatistics.idStatisticBase SET value = ? WHERE idCharacter = ? AND statisticsbases.name = ?;", [number, idCharacter, statName]);
+    }
+
+
+}
+
+module.exports = PStatistics;
---
+++ bin/Achievement/Statistics.js
@@ -0,0 +1,5 @@
+class Statistics {
+
+}
+
+module.exports = Statistics;
