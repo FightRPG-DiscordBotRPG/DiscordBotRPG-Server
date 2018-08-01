4541d5e521e1f5ef4bb38c36912e7018112bcabc
Jean-Charles TECHER <techer.jeancharles974@gmail.com>
2018-08-01 19:38:30 +0400

    + Ajout server stats

--- bin/DatabaseInitializer.js
+++ bin/DatabaseInitializer.js
@@ -1,5 +1,5 @@
 const conn = require("../conf/mysql");
-
+const mysql = require("mysql");
 
 class DatabaseInitializer {
     static initialize() {
@@ -18,6 +18,36 @@ class DatabaseInitializer {
 
     static PStats() {
         conn.query("INSERT IGNORE INTO `charactersstatistics`(`idStatisticBase`, `idCharacter`, `value`) SELECT statisticsbases.idStatisticBase, characters.idCharacter, 0 as value FROM statisticsbases JOIN characters");
+    }
+
+    static serversStats(guilds) {
+        let guildsAddString = "";
+        let i = 0;
+
+        guilds.forEach( (guild, snowflake, map) => {
+            i++;
+            //console.log(guild.id + " " + guild.name + " " + guild.memberCount + " " + guild.region);
+            //guildsAddString += "(" + guild.id + "," + "`::`, `" + guild.name + "`," + guild.memberCount + ",`" + guild.region + "`";
+            let temp = "(?, '::', ?, ?, ?)";
+            temp = mysql.format(temp, [guild.id, guild.name, guild.memberCount, guild.region]);
+            if(i < guilds.size) {
+                temp += ",";
+            } else {
+                temp += ";";
+            }
+            guildsAddString += temp;
+        });
+
+        conn.query("INSERT IGNORE INTO serversstats (idServer, serverPrefix, serverName, memberCount, region) VALUES " + guildsAddString);
+
+        // affectedRows => number of rows that was added
+
+    }
+
+    static newGuild(guild) {
+        let temp = "(?, '::', ?, ?, ?)";
+        temp = mysql.format(temp, [guild.id, guild.name, guild.memberCount, guild.region]);
+        conn.query("INSERT IGNORE INTO serversstats (idServer, serverPrefix, serverName, memberCount, region) VALUES " + temp + ";");
     }
 }
 
