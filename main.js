a61e6da5e04ea2d112baf81aadd4186edd0b2a64
Jean-Charles TECHER <techer.jeancharles974@gmail.com>
2018-08-01 19:37:20 +0400

    + Ajout database add guild + Ajout temps de load

--- main.js
+++ main.js
@@ -13,36 +13,52 @@ const DatabaseInitializer = require("./bin/DatabaseInitializer");
 var bot = new Discord.Client();
 var prefix = "::";
 
+var timeStart = Date.now();
+let syncStartWith = Date.now();
+let totalGameStartTime = Date.now();
+
 process.on('unhandledRejection', up => { throw up });
 
+console.log("Initializing Database ...");
 DatabaseInitializer.initialize();
+console.log("Database initialized, took : " + ((Date.now() - syncStartWith) / 1000) + " seconds");
 
 console.log("Bot Starting ...");
 
-console.log(Globals);
+//console.log(Globals);
 
 bot.on("ready", () => {
+    console.log("Bot Connected");
+    
     bot.user.setPresence({
         game: {
             name: "On " + bot.guilds.size + " guilds !",
         },
     });
 
-    console.log("Bot Ready");
+    
     if(conf.env === "prod") {
         const DBL = require("dblapi.js");
         const dbl = new DBL(conf.discordbotskey, bot);
 
         setInterval(() => {
+            console.log("Sending stats to https://discordbots.org/ ...");
             dbl.postStats(bot.guilds.size);
+            console.log("Data sent");
         }, 1800000);
     }
+    
+    console.log("Loading servers stats ...");
+    DatabaseInitializer.serversStats(bot.guilds);
+    console.log("Servers stats loaded");
+
+    console.log("Bot ready");
+    console.log("Bot load finished, took : " + ((Date.now() - timeStart) / 1000) + " seconds");
 
 });
 
 // Key Don't open
 bot.login(Key);
-
 
 // UNDER CONSTRUCTION SUBJECT TO CHANGE
 var connectedUsers = {};
@@ -50,11 +66,25 @@ var connectedGuilds = {};
 
 Globals.connectedUsers = connectedUsers;
 Globals.connectedGuilds = connectedGuilds;
+
+syncStartWith = Date.now();
+console.log("Loading Areas...");
 Globals.areasManager = new AreasManager();
+console.log("Areas loaded, took : " + ((Date.now() - syncStartWith) / 1000) + " seconds");
+
+
+syncStartWith = Date.now();
+console.log("Loading Fight Manager...");
 Globals.fightManager = new FightManager();
+console.log("Fight Manager loaded, took : " + ((Date.now() - syncStartWith) / 1000) + " seconds");
 Globals.discordClient = bot;
 
+syncStartWith = Date.now();
+console.log("Loading Commands module...");
 var ChatReceiver = new Commandes(prefix);
+console.log("Commands module loaded, took : " + ((Date.now() - syncStartWith) / 1000) + " seconds");
+
+console.log("Game Loaded, took : " + ((Date.now() - totalGameStartTime) / 1000) + " seconds");
 
 ChatReceiver.bot = bot;
 ChatReceiver.fightManager = Globals.fightManager;
@@ -85,12 +115,13 @@ bot.on('message', (message) => {
 
 });
 
-bot.on('guildCreate', () => {
+bot.on('guildCreate', (guild) => {
     bot.user.setPresence({
         game: {
             name: "On " + bot.guilds.size + " guilds !",
         },
     });
+    DatabaseInitializer.newGuild(guild);
 });
 
 bot.on('guildDelete', () => {
@@ -104,6 +135,6 @@ bot.on('guildDelete', () => {
 
 
 // Load api after all 
-const ApiResponder = require("./api/ApiResponder.js");
+//const ApiResponder = require("./api/ApiResponder.js");
 
 
