c559960acc9c4b44a185b8e4d50e3260f4d86994
Jean-Charles TECHER <techer.jeancharles974@gmail.com>
2018-08-30 14:17:29 +0400

    + Add use function

--- bin/Items/Consumable.js
+++ bin/Items/Consumable.js
@@ -4,6 +4,10 @@ class Consumable extends Item {
     constructor(id) {
         super(id);
     }
+
+    use(character) {
+        throw "Use must be implemented";
+    }
 }
 
 module.exports = Consumable;
