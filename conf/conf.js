4e1c54962f2b285e0005346088f56e52f2e3f6fa
Jean-Charles TECHER <techer.jeancharles974@gmail.com>
2018-09-19 15:32:32 +0400

    + ajout clé webhook

--- conf/conf.js
+++ conf/conf.js
@@ -1,6 +1,7 @@
 const conf = {
     env: "dev",
-    discordbotskey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQwMTQyMTY0NDk2ODYyNDEyOSIsImJvdCI6dHJ1ZSwiaWF0IjoxNTMwMjc0MDIyfQ.PIlRw8c0L_cCWkcS7JM3XJNpv6Vj9RymEvbsvIysTFI"
+    discordbotskey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQwMTQyMTY0NDk2ODYyNDEyOSIsImJvdCI6dHJ1ZSwiaWF0IjoxNTMwMjc0MDIyfQ.PIlRw8c0L_cCWkcS7JM3XJNpv6Vj9RymEvbsvIysTFI",
+    webhookkey: "UYI4ENyZkL"
 }
 
 module.exports = conf;
