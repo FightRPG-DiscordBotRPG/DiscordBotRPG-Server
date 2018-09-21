a04a858327760a2e0c66aafb12d8c693af8385b5
Jean-Charles TECHER <techer.jeancharles974@gmail.com>
2018-09-21 15:48:10 +0400

    * Update shard

--- mainForShard.js
+++ mainForShard.js
@@ -1,31 +1,52 @@
 const Discord = require('discord.js');
-const Globals = require("./bin/Globals");
-const FightManager = require("./bin/FightManager");
-const AreasManager = require("./bin/Areas/AreasManager.js");
 const conf = require("./conf/conf");
 const DatabaseInitializer = require("./bin/DatabaseInitializer");
+const User = require("./bin/User");
+const LootSystem = require("./bin/LootSystem");
 
 let timeStart = Date.now();
 let syncStartWith = Date.now();
 
-process.on('unhandledRejection', up => { throw up });
+process.on('unhandledRejection', up => {
+    throw up
+});
 
 console.log("Initializing Database ...");
 DatabaseInitializer.initialize();
 console.log("Database initialized, took : " + ((Date.now() - syncStartWith) / 1000) + " seconds");
 
-syncStartWith = Date.now();
-console.log("Loading Areas...");
-var areasManager = new AreasManager();
-Globals.areasManager = areasManager;
-console.log("Areas loaded, took : " + ((Date.now() - syncStartWith) / 1000) + " seconds");
+if (conf.env === "prod") {
+    const options = {
+        webhookPort: 5000,
+        webhookAuth: conf.webhookkey
+    };
+    const dbl = new DBL(conf.discordbotskey, options, bot);
+    dbl.webhook.on('ready', hook => {
+        console.log(`Webhook running at http://${hook.hostname}:${hook.port}${hook.path}`);
 
+    });
+    dbl.webhook.on('vote', vote => {
+        if (User.exist(vote.user)) {
+            let user = new User(vote.user);
+            user.loadUser();
+            let ls = new LootSystem();
+            ls.giveToPlayer(user.character, 41, 1, vote.isWeekend ? 2 : 1);
+            let duser = bot.users.get(vote.user);
+            if (duser != null) {
+                let lang = user.getLang();
+                let msg = Translator.getString(lang, "vote_daily", "you_voted");
+                if (vote.isWeekend) {
+                    msg += Translator.getString(lang, "vote_daily", "vote_week_end");
+                } else {
+                    msg += Translator.getString(lang, "vote_daily", "vote_no_week_end");
+                }
+                duser.send(msg);
+            }
+        }
 
-syncStartWith = Date.now();
-console.log("Loading Fight Manager...");
-var fightManager = new FightManager();
-Globals.fightManager = fightManager;
-console.log("Fight Manager loaded, took : " + ((Date.now() - syncStartWith) / 1000) + " seconds");
+    });
+
+}
 
 syncStartWith = Date.now();
 console.log("Loading Shards...");
@@ -35,12 +56,10 @@ Manager.spawn(2).then(() => {
     console.log("Shards loaded, took : " + ((Date.now() - syncStartWith) / 1000) + " seconds");
     console.log("The bot and all of his shards are loaded, took : " + ((Date.now() - timeStart) / 1000) + " seconds");
     Manager.on("message", (shard, message) => {
-        //console.log(message.type);
+        console.log(message);
     });
 }); // This example will spawn 2 shards (5,000 guilds);
 
 
 
 const ApiResponder = require("./api/ApiResponder.js");
-
-
--- shardedMain.js
+++ shardedMain.js
@@ -1,84 +1,90 @@
-'use strict';
-const Commandes = require("./bin/Commandes.js")
+const Globals = require("./bin/Globals.js");
+const Translator = require("./bin/Translator/Translator");
 const Discord = require("discord.js");
 const Key = require("./conf/botkey.js");
 const FightManager = require("./bin/FightManager");
-const Globals = require("./bin/Globals.js");
 const crypto = require("crypto");
 const AreasManager = require("./bin/Areas/AreasManager.js");
 const conf = require("./conf/conf");
 const DatabaseInitializer = require("./bin/DatabaseInitializer");
-
-const redis = require("redis");
-var redisClient = redis.createClient();
+const ModuleHandler = require("./bin/Modules/ModuleHandler");
+const DBL = require("dblapi.js");
+const dbl = new DBL(conf.discordbotskey, bot);
 
 var bot = new Discord.Client();
-var prefix = "::";
 
-let timeStart = Date.now();
+var timeStart = Date.now();
 let syncStartWith = Date.now();
 let totalGameStartTime = Date.now();
 
-console.log("Bot Starting ...");
+process.on('unhandledRejection', up => {
+    throw up
+});
+
+
+console.log("Shard id: " + bot.shard.id + " Starting ...");
 
 //console.log(Globals);
 
 bot.on("ready", () => {
     console.log("Bot Connected");
-    
+
     bot.user.setPresence({
         game: {
             name: "On " + bot.guilds.size + " guilds !",
         },
     });
 
-    
-    if(conf.env === "prod") {
-        const DBL = require("dblapi.js");
-        const dbl = new DBL(conf.discordbotskey, bot);
+    if (conf.env === "prod") {
 
         setInterval(() => {
-            console.log("Sending stats to https://discordbots.org/ ...");
-            dbl.postStats(bot.guilds.size);
+            console.log("(Shard : " + bot.shard.id + ")Sending stats to https://discordbots.org/ ...");
+            dbl.postStats(bot.guilds.size, bot.shard.id, bot.shard.count);
             console.log("Data sent");
         }, 1800000);
     }
-    
+
     console.log("Loading servers stats ...");
     DatabaseInitializer.serversStats(bot.guilds);
     console.log("Servers stats loaded");
 
-    console.log("Bot ready");
-    console.log("Bot load finished, took : " + ((Date.now() - timeStart) / 1000) + " seconds");
+    console.log("Shard : " + bot.shard.id + " ready");
+    console.log("Shard : " + bot.shard.id + " load finished, took : " + ((Date.now() - timeStart) / 1000) + " seconds");
 
 });
 
 // Key Don't open
 bot.login(Key);
 
-// UNDER CONSTRUCTION SUBJECT TO CHANGE
+let moduleHandler = new ModuleHandler();
 
-Globals.discordClient = bot;
+// UNDER CONSTRUCTION SUBJECT TO CHANGE
+var connectedUsers = {};
+var connectedGuilds = {};
+
+Globals.connectedUsers = connectedUsers;
+Globals.connectedGuilds = connectedGuilds;
 
 syncStartWith = Date.now();
-console.log("Loading Commands module...");
-var ChatReceiver = new Commandes(prefix);
-console.log("Commands module loaded, took : " + ((Date.now() - syncStartWith) / 1000) + " seconds");
-
-console.log("Game Loaded, took : " + ((Date.now() - totalGameStartTime) / 1000) + " seconds");
-
-ChatReceiver.bot = bot;
-ChatReceiver.fightManager = Globals.fightManager;
-ChatReceiver.connectedUsers = Globals.connectedUsers;
-ChatReceiver.connectedGuilds = Globals.connectedGuilds;
-ChatReceiver.areasManager = Globals.areasManager;
-console.log(Globals.areasManager);
+console.log("Loading Areas...");
+Globals.areasManager = new AreasManager();
+console.log("Areas loaded, took : " + ((Date.now() - syncStartWith) / 1000) + " seconds");
 
 
-bot.on('message', (message) => {
+syncStartWith = Date.now();
+console.log("Loading Fight Manager...");
+Globals.fightManager = new FightManager();
+console.log("Fight Manager loaded, took : " + ((Date.now() - syncStartWith) / 1000) + " seconds");
+Globals.discordClient = bot;
+
+console.log("Game World loaded, took : " + ((Date.now() - totalGameStartTime) / 1000) + " seconds");
+
+
+
+
+bot.on('message', async (message) => {
     try {
-        console.log(message.client.shard.id);
-        ChatReceiver.reactTo(message);
+        await moduleHandler.run(message);
     } catch (err) {
         let msgError = "Oops something goes wrong, report the issue here (https://github.com/FightRPG-DiscordBotRPG/FightRPG-Discord-BugTracker/issues)\n";
 
@@ -92,7 +98,7 @@ bot.on('message', (message) => {
         console.log(err);
         message.channel.send(msgError).catch((e) => null);
     }
-    
+
 });
 
 bot.on('guildCreate', (guild) => {
@@ -111,10 +117,3 @@ bot.on('guildDelete', () => {
         },
     });
 });
-
-
-
-// Load api after all 
-//const ApiResponder = require("./api/ApiResponder.js");
-
-
