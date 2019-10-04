8a2ad2ec856ff580a6112e0812e4f0563121138c
Jean-Charles TECHER <techer.jeancharles974@gmail.com>
2019-10-04 20:42:20 +0200

    * Update 1.8.2 * Fix bug when regions are untranslated

--- bin/Guild.js
+++ bin/Guild.js
@@ -248,17 +248,41 @@ class Guild {
 
     }
 
+    // Fix when not translated
     async getTerritories(lang = "en") {
-        let res = await conn.query("SELECT nameArea, nameRegion FROM areasowners INNER JOIN areasregions ON areasregions.idArea = areasowners.idArea INNER JOIN localizationareas ON localizationareas.idArea = areasowners.idArea INNER JOIN localizationregions ON localizationregions.idRegion = areasregions.idRegion WHERE idGuild = ? AND localizationregions.lang = ? AND localizationareas.lang = ? ORDER BY nameRegion;", [this.id, lang, lang]);
+        let request = "SELECT areasowners.idArea, nameArea, nameRegion FROM areasowners INNER JOIN areasregions ON areasregions.idArea = areasowners.idArea INNER JOIN localizationareas ON localizationareas.idArea = areasowners.idArea INNER JOIN localizationregions ON localizationregions.idRegion = areasregions.idRegion WHERE idGuild = ? AND localizationregions.lang = ? AND localizationareas.lang = ? ORDER BY areasowners.idArea;";
+
+        // First get number of territories own by the guild
+        let numberOfOnwedTerritories = await conn.query("SELECT COUNT(*) as number FROM areasowners WHERE idGuild = ?;", [this.id]);
+
+        // Then we get the list
+        let res = await conn.query(request, [this.id, lang, lang]);
+        let englishRes = [];
+
+        // Then if the number is incorrect we get the english version
+        if (lang != "en" && res.length < numberOfOnwedTerritories[0].number) {
+            // If an area or a region is not translated 
+            englishRes = await conn.query(request, [this.id, "en", "en"]);
+        }
 
         let territories = {};
-        for (let territory of res) {
+        let indexShift = 0
+        let actualIndex = 0
+        for (let territory of englishRes) {
+            if (res.length > 0 && res[actualIndex - indexShift].idArea == territory.idArea) {
+                territory = res[actualIndex - indexShift];
+            } else {
+                indexShift++;
+            }
+
             if (territories[territory.nameRegion] == null) {
                 territories[territory.nameRegion] = [];
             }
             territories[territory.nameRegion].push(territory.nameArea);
+
+            actualIndex++
         }
-        return { totalNumberOfTerritories: res.length, territories: territories };
+        return { totalNumberOfTerritories: numberOfOnwedTerritories, territories: territories };
     }
 
     async toApi() {
--- conf/versions.js
+++ conf/versions.js
@@ -1,4 +1,4 @@
 module.exports = {
-    server: "1.8.1",
+    server: "1.8.2",
     website: null,
 }
