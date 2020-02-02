47fb133e1683458128380dbea7a7a6ae44a66ab3
Roncarlos974 <9269672+Roncarlos@users.noreply.github.com>
2020-02-02 15:51:14 +0100

    remove gitignore files (sql)

--- sql/Donjons&boss/itemsdonjon.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `itemsbase` (`idBaseItem`, `nomItem`, `descItem`, `idType`, `idRarity`, `imageItem`)
-VALUES (NULL, 'Fouet de enflammé', NULL, '1', '5', 'unknown'), (NULL, 'Casque en cornes de flammes ', NULL, '4', '5', 'unknown'),
-(NULL, 'Ceinture de flammes', NULL, '3', '5', 'unknown'), (NULL, 'Armure de flammes', NULL, '2', '5', 'unknown');
-
-INSERT INTO `areasitems` (`idArea`, `idBaseItem`) VALUES ('7', '26'), ('7', '25'), ('7', '24'), ('7', '23');
--- sql/Update_1_80/ResetGlobal.sql
+++
@@ -1,8 +0,0 @@
--- Guilds
-
-UPDATE guilds SET guilds.level = 1, argent = 0;
-UPDATE characters SET characters.money = 0, idArea = 1;
-UPDATE charactershonor SET Honor = 0;
-UPDATE charactersstatistics SET charactersstatistics.value = 0;
-TRUNCATE TABLE commandslogs;
-INSERT INTO charactersachievements SELECT characters.idCharacter, 1 FROM characters;
