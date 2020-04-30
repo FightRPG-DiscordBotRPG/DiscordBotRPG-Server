61c162e3f5a3dbc3ba8876f3b37f14c5347b9038
Roncarlos974 <9269672+Roncarlos@users.noreply.github.com>
2020-04-30 17:18:15 +0200

    fix missing name when equip item

--- bin/Modules/Modules/EquipmentModule.js
+++ bin/Modules/Modules/EquipmentModule.js
@@ -79,7 +79,7 @@ class EquipmentModule extends GModule {
                                 if (swapItem > 0) {
                                     await Globals.connectedUsers[res.locals.id].character.getInv().addToInventory(swapItem);
                                 }
-                                data.success = Translator.getString(res.locals.lang, "inventory_equipment", "item_equiped");
+                                data.success = Translator.getString(res.locals.lang, "inventory_equipment", "item_equiped", [itemToEquip.getName(data.lang)]);
                             } else {
                                 data.error = Translator.getString(res.locals.lang, "errors", "item_cant_equip_higher_level", [itemToEquip.getLevel()]);
                             }
