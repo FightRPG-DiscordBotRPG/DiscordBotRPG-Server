0609dd41ddfcfdc2c83f3cbf19263ad3db5e8bc7
Roncarlos974 <9269672+Roncarlos@users.noreply.github.com>
2020-06-23 09:59:52 +0200

    add example conf

---
+++ conf/conf.example.js
@@ -0,0 +1,20 @@
+const conf = {
+    env: "dev",
+    discordbotskey: "",
+    webhookkey: "",
+    registerkey: "DEFAULT_KEY",
+    database: {
+        host: "localhost",
+        user: "root", password: "",
+        database: "discord_bot_rpg",
+        charset: "utf8mb4_unicode_ci",
+        multipleStatements: true
+    },
+    tournamentFilesPath: "F:\\FightRPG Tournament Datas\\",
+    discordBotAddresses: [
+        "127.0.0.1"
+    ],
+};
+
+
+module.exports = conf;
