784e9f4f73a3fd5b8c5054c51018510d5af51ce7
Roncarlos974 <9269672+Roncarlos@users.noreply.github.com>
2020-09-23 18:56:37 +0200

    added states and skills

--- tools/PSTree Creator Backend/main.js
+++ tools/PSTree Creator Backend/main.js
@@ -2,6 +2,9 @@ const conf = require("../../conf/conf");
 const conn = require("../../conf/mysql");
 const Nodes = require("../../bin/PSTree/Nodes");
 const Globals = require("../../bin/Globals");
+const Skill = require("../../bin/SkillsAndStatus/Skill");
+const Translator = require("../../bin/Translator/Translator");
+const State = require("../../bin/SkillsAndStatus/State");
 
 const express = require("express"),
     app = express(),
@@ -23,12 +26,48 @@ async function Start() {
     // Load Object before 
     await nodesObject.load();
     await Globals.loadGlobals();
+    await Translator.load();
     app.get("/nodes", async (req, res) => {
         res.json(await nodesObject.toApi());
     });
 
     app.get("/nodes/visuals", async (req, res) => {
         res.json(await nodesObject.visualsToApi());
+    });
+
+    app.get("/skills", async (req, res) => {
+        let results = await conn.query("SELECT idSkill FROM skills");
+        let promises = [];
+        let skills = [];
+        for (let data of results) {
+            let skill = new Skill();
+            skills.push(skill);
+            promises.push((async (skillFunc) => {
+                await skillFunc.loadWithID(data.idSkill);
+                skillFunc.name = skillFunc.getName();
+            })(skill));
+        }
+
+        await Promise.all(promises);
+
+        res.json({ skills: skills });
+    });
+
+    app.get("/states", async (req, res) => {
+        let results = await conn.query("SELECT idState FROM states");
+        let promises = [];
+        let states = [];
+        for (let data of results) {
+            let state = new State();
+            states.push(state);
+            promises.push((async (stateFunc) => {
+                await stateFunc.loadWithID(data.idState);
+            })(state));
+        }
+
+        await Promise.all(promises);
+
+        res.json({ skills: states.map(s => s.toApi()) });
     });
 }
 
