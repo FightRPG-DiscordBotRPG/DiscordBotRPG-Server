daf69f7cd409d826f1be78eec3676f99b7ea2477
Roncarlos974 <9269672+Roncarlos@users.noreply.github.com>
2021-01-17 19:08:12 +0100

    added reforge to item & moving stats generation to item

--- bin/Items/Item.js
+++ bin/Items/Item.js
@@ -102,12 +102,248 @@ class Item {
         }
     }
 
+    static getStatsNumber(rarity) {
+        if (rarity < 6) {
+            return rarity - 1 + this.numberOfStatsBonus(rarity);
+        } else {
+            return Object.keys(Globals.statsIdsByName).length;
+        }
+    }
+
+    static getSecondaryStatsNumber(rarity) {
+        return rarity + this.numberOfStatsBonus(rarity + 2);
+    }
+
+    static numberOfStatsBonus(rarity) {
+        let maxPossible = 5 - rarity;
+        maxPossible = maxPossible < 0 ? 0 : maxPossible;
+        let maxStats = 0;
+        while (maxPossible > 0) {
+            if (Math.random() < 0.1) {
+                maxStats++;
+            }
+            maxPossible--;
+        }
+        return maxStats;
+    }
+
+    static getRandomStatRatio(rarity, maxToPerfection = 100) {
+        if (rarity < 6) {
+            let min = (rarity - 1) / 5;
+            let max = rarity / 5;
+            return Math.random() * (max - min) + min;
+        } else {
+            return 1;
+        }
+    }
+
+    /**
+     * 
+     * @param {string} objectSubtype
+     * @param {string} statName
+     * @param {number} level
+     * @param {number} ratio
+     */
+    static getStatValue(objectSubtype, statName, level, ratio) {
+
+        let baseValue;
+
+        switch (statName) {
+            case Stats.possibleStats.Armor:
+                baseValue = Math.ceil((8 * (Math.pow(level, 2)) / 7) * ratio / 4.5);
+                break;
+            default:
+                baseValue = Math.ceil(level * ratio * 2);
+                break;
+
+        }
+
+        let multiplier = Stats.ratiosBasedOnSubtype[statName] && Stats.ratiosBasedOnSubtype[statName][objectSubtype] ? Stats.ratiosBasedOnSubtype[statName][objectSubtype] : 1;
+        
+        return Math.ceil(baseValue * multiplier);
+
+    }
+
+    /**
+     * 
+     * @param {string} secondaryStatName
+     * @param {number} level
+     * @param {number} ratio
+     */
+    static getSecondaryStatValue(objectSubtype, secondaryStatName, level, ratio) {
+
+        let baseValue;
+        switch (secondaryStatName) {
+            case SecondaryStats.possibleStats.CriticalRate:
+            case SecondaryStats.possibleStats.EvadeRate:
+            case SecondaryStats.possibleStats.HitRate:
+            case SecondaryStats.possibleStats.MagicalEvadeRate:
+            case SecondaryStats.possibleStats.CritcalEvadeRate:
+                baseValue = Utils.randRangeInt(3, 5) * ratio;
+                break;
+            case SecondaryStats.possibleStats.RegenEnergy:
+                baseValue = Utils.randRangeInt(1, 3) * ratio;
+                break;
+            case SecondaryStats.possibleStats.RegenHp:
+            case SecondaryStats.possibleStats.RegenMp:
+                baseValue = Utils.randRangeInt(1, level * 1.25) * ratio;
+                break;
+            case SecondaryStats.possibleStats.SkillEnergyCost:
+            case SecondaryStats.possibleStats.SkillManaCost:
+                baseValue = Utils.randRangeInt(2, 5) * ratio;
+                break;
+            case SecondaryStats.possibleStats.Threat:
+            case SecondaryStats.possibleStats.Initiative:
+                baseValue = Utils.randRangeInt(1, level / 2);
+                break;
+            case SecondaryStats.possibleElementalResists.Air:
+            case SecondaryStats.possibleElementalResists.Dark:
+            case SecondaryStats.possibleElementalResists.Earth:
+            case SecondaryStats.possibleElementalResists.Fire:
+            case SecondaryStats.possibleElementalResists.Water:
+            case SecondaryStats.possibleElementalResists.Light:
+                baseValue = Utils.randRangeInt(1, 5) * ratio;
+                break;
+            default:
+                baseValue = 1;
+                break;
+
+        }
+
+        let multiplier = SecondaryStats.ratiosBasedOnSubtype[secondaryStatName] && SecondaryStats.ratiosBasedOnSubtype[secondaryStatName][objectSubtype] ? SecondaryStats.ratiosBasedOnSubtype[secondaryStatName][objectSubtype] : 1;
+
+        return Math.ceil(baseValue * multiplier);
+
+    }
+
+    /**
+     * 
+     * @param {any} idItem
+     * @param {Stats} stats
+     * @param {SecondaryStats} secondaryStats
+     */
+    static async replaceAllStats(idItem, stats, secondaryStats) {
+        let statsValues = [];
+        let secondaryStatsValues = [];
+        let secondaryStatsElementalValues = [];
+
+        // Preparing Stats
+        for (let i in Globals.statsIdsByName) {
+            statsValues.push([idItem, parseInt(Globals.statsIdsByName[i]), stats[i]]);
+        }
+
+        // Preparing Secondary Stats
+        for (let i in Globals.secondaryStatsIdsByName) {
+            secondaryStatsValues.push([idItem, parseInt(Globals.secondaryStatsIdsByName[i]), secondaryStats[i]]);
+        }
+
+        // Preparing Elemental Resists
+        for (let i in Globals.elementsTypesIdsByName) {
+            secondaryStatsElementalValues.push([idItem, parseInt(Globals.elementsTypesIdsByName[i]), secondaryStats[i + "Resist"]]);
+        }
+
+        await Promise.all([
+            conn.query("REPLACE INTO itemsstats VALUES ?;", [statsValues]),
+            conn.query("REPLACE INTO itemssecondarystats VALUES ?;", [secondaryStatsValues]),
+            secondaryStatsElementalValues.length > 0 ? conn.query("REPLACE INTO itemssecondarystatselementalresists VALUES ?;", [secondaryStatsElementalValues]) : null,
+        ]);
+    }
+
+    /**
+     * 
+     * @param {number} rarity
+     * @param {string} objectType
+     * @param {string} objectSubtype
+     * @param {number} level
+     * @param {any} maxStatsPercentage
+     */
+    static generateItemsStats(rarity, objectType, objectSubtype, level, maxStatsPercentage = 100) {
+        let stats = new Stats(0);
+        let secondaryStats = new SecondaryStats(0);
+
+        let statsPossible = Object.keys(Globals.statsIdsByName);
+        let secondaryStatsPossible = [...Globals.allSecondaryStatsNames];
+        let numberOfStats = this.getStatsNumber(rarity);
+        let numberOfSecondaryStats = this.getSecondaryStatsNumber(rarity);
+
+        let ratio = this.getRandomStatRatio(rarity, maxStatsPercentage);
+
+        if (objectType == "weapon") {
+            //Une arme
+            stats.strength = this.getStatValue(objectSubtype, Stats.possibleStats.Strength, level, ratio);
+            statsPossible.splice(statsPossible.indexOf("strength"), 1);
+        } else {
+            stats.armor = this.getStatValue(objectSubtype, Stats.possibleStats.Armor, level, ratio);
+            statsPossible.splice(statsPossible.indexOf("armor"), 1);
+        }
+
+        while (numberOfStats > 0 && statsPossible.length > 0) {
+            ratio = this.getRandomStatRatio(rarity, maxStatsPercentage);
+            let r = statsPossible[Math.floor(Math.random() * statsPossible.length)];
+
+            stats[r] = this.getStatValue(objectSubtype, r, level, ratio);
+
+            statsPossible.splice(statsPossible.indexOf(r), 1);
+            numberOfStats--;
+        }
+
+        while (numberOfSecondaryStats > 0 && secondaryStatsPossible.length > 0) {
+            ratio = this.getRandomStatRatio(rarity, maxStatsPercentage);
+
+            let r = secondaryStatsPossible[Math.floor(Math.random() * secondaryStatsPossible.length)];
+
+            secondaryStats[r] = this.getSecondaryStatValue(objectSubtype, r, level, ratio);
+
+            // Not a bonus then
+            if (Math.random() <= 0.2) {
+                secondaryStats[r] = -secondaryStats[r];
+
+                if (Math.random() <= 0.4 && secondaryStatsPossible.length - 1 > 0) {
+
+                    if (Math.random() <= 0.1 && secondaryStatsPossible.length - 2 > 0) {
+                        numberOfSecondaryStats++;
+                    }
+
+                    numberOfSecondaryStats++;
+                }
+            }
+
+            secondaryStatsPossible.splice(statsPossible.indexOf(r), 1);
+            numberOfSecondaryStats--;
+        }
+
+        return { stats: stats, secondaryStats: secondaryStats };
+    }
+
     getLevel() {
         return this.level;
     }
 
     getIdRarity() {
         return this.idRarity;
+    }
+
+    async reforge() {
+        if (!Item.canHaveStats(this.typeName)) {
+            return;
+        }
+        let newStats = Item.generateItemsStats(this.idRarity, this.typeName, this.subTypeName, this.getLevel());
+        this.stats = newStats.stats;
+        this.secondaryStats = newStats.secondaryStats;
+        await Item.replaceAllStats(this.id, newStats.stats, newStats.secondaryStats);
+        await this.updatePower();
+    }
+
+    static canHaveStats(type) {
+        switch (type) {
+            case "weapon":
+            case "chest":
+            case "legs":
+            case "head":
+                return true;
+            default:
+                return false;
+        }
     }
 
     /**
@@ -361,3 +597,4 @@ const PolarBear = require("./Mounts/PolarBear");
 const EnergyPotion = require("./Potions/EnergyPotion");
 const Stats = require("../Stats/Stats");
 const SecondaryStats = require("../Stats/Secondary/SecondaryStats");
+const Utils = require("../Utilities/Utils.js");
--- bin/LootSystem.js
+++ bin/LootSystem.js
@@ -166,185 +166,38 @@ class LootSystem {
         return false;
     }
 
-    /**
-     * 
-     * @param {string} objectSubtype
-     * @param {string} statName
-     * @param {number} level
-     * @param {number} ratio
-     */
-    getStatValue(objectSubtype, statName, level, ratio) {
-
-        let baseValue;
-
-        switch (statName) {
-            case Stats.possibleStats.Armor:
-                baseValue = Math.ceil((8 * (Math.pow(level, 2)) / 7) * ratio / 4.5);
-                break;
-            default:
-                baseValue = Math.ceil(level * ratio * 2);
-                break;
-
-        }
-
-        let multiplier = Stats.ratiosBasedOnSubtype[statName] && Stats.ratiosBasedOnSubtype[statName][objectSubtype] ? Stats.ratiosBasedOnSubtype[statName][objectSubtype] : 1;
-
-        return Math.ceil(baseValue * multiplier);
-
-    }
-
-    /**
-     * 
-     * @param {string} secondaryStatName
-     * @param {number} level
-     * @param {number} ratio
-     */
-    getSecondaryStatValue(objectSubtype, secondaryStatName, level, ratio) {
-
-        let baseValue;
-
-        switch (secondaryStatName) {
-            case SecondaryStats.possibleStats.CriticalRate:
-            case SecondaryStats.possibleStats.EvadeRate:
-            case SecondaryStats.possibleStats.HitRate:
-            case SecondaryStats.possibleStats.MagicalEvadeRate:
-            case SecondaryStats.possibleStats.CritcalEvadeRate:
-                baseValue = Utils.randRangeInt(3, 5) * ratio;
-                break;
-            case SecondaryStats.possibleStats.RegenEnergy:
-                baseValue = Utils.randRangeInt(1, 3) * ratio;
-                break;
-            case SecondaryStats.possibleStats.RegenHp:
-            case SecondaryStats.possibleStats.RegenMp:
-                baseValue = Utils.randRangeInt(1, level*1.25) * ratio;
-                break;
-            case SecondaryStats.possibleStats.SkillEnergyCost:
-            case SecondaryStats.possibleStats.SkillManaCost:
-                baseValue = Utils.randRangeInt(2, 5) * ratio;
-                break;
-            case SecondaryStats.possibleStats.Threat:
-            case SecondaryStats.possibleStats.Initiative:
-                baseValue = Utils.randRangeInt(1, level / 2);
-                break;
-            case SecondaryStats.possibleElementalResists.Air:
-            case SecondaryStats.possibleElementalResists.Dark:
-            case SecondaryStats.possibleElementalResists.Earth:
-            case SecondaryStats.possibleElementalResists.Fire:
-            case SecondaryStats.possibleElementalResists.Water:
-            case SecondaryStats.possibleElementalResists.Light:
-                baseValue = Utils.randRangeInt(1, 5) * ratio;
-                break;
-            default:
-                baseValue = 1;
-                break;
-
-        }
-
-        let multiplier = SecondaryStats.ratiosBasedOnSubtype[secondaryStatName] && SecondaryStats.ratiosBasedOnSubtype[secondaryStatName][objectSubtype] ? SecondaryStats.ratiosBasedOnSubtype[secondaryStatName][objectSubtype] : 1;
-
-        return Math.ceil(baseValue * multiplier);
-
-    }
+    
 
     // Return id of new item if created
-    async newItem(idBase, level, maxStatsPercentage = 100) {
+    async newItem(idBase, level) {
         let res = await conn.query(`SELECT * FROM itemsbase INNER JOIN itemstypes ON itemstypes.idType = itemsbase.idType INNER JOIN itemssoustypes ON itemssoustypes.idSousType = itemsbase.idSousType WHERE itemsbase.idBaseItem = ?`, [idBase]);
         if (res[0]) {
             let rarity = res[0].idRarity;
             let stats = new Stats(0);
             let secondaryStats = new SecondaryStats(0);
 
-            let statsPossible = Object.keys(Globals.statsIdsByName);
-            let secondaryStatsPossible = [...Globals.allSecondaryStatsNames];
-            let numberOfStats = this.getStatsNumber(rarity);
-            let numberOfSecondaryStats = this.getSecondaryStatsNumber(rarity);
             let objectType = res[0]["nomType"];
             let objectSubtype = res[0]["nomSousType"];
             let equipable = res[0]["equipable"];
-            maxStatsPercentage = maxStatsPercentage >= 50 ? maxStatsPercentage : 100;
 
-            if (equipable == true && LootSystem.canHaveStats(objectType)) {
-                let ratio = this.getRandomStatRatio(rarity, maxStatsPercentage);
-
-                if (objectType == "weapon") {
-                    //Une arme
-                    stats.strength = this.getStatValue(objectSubtype, Stats.possibleStats.Strength, level, ratio);
-                    statsPossible.splice(statsPossible.indexOf("strength"), 1);
-                } else {
-                    stats.armor = this.getStatValue(objectSubtype, Stats.possibleStats.Armor, level, ratio);
-                    statsPossible.splice(statsPossible.indexOf("armor"), 1);
-                }
-
-                while (numberOfStats > 0 && statsPossible.length > 0) {
-                    ratio = this.getRandomStatRatio(rarity, maxStatsPercentage);
-                    let r = statsPossible[Math.floor(Math.random() * statsPossible.length)];
-
-                    stats[r] = this.getStatValue(objectSubtype, r, level, ratio);
-
-                    statsPossible.splice(statsPossible.indexOf(r), 1);
-                    numberOfStats--;
-                }
-
-                while (numberOfSecondaryStats > 0 && secondaryStatsPossible.length > 0) {
-                    ratio = this.getRandomStatRatio(rarity, maxStatsPercentage);
-
-                    let r = secondaryStatsPossible[Math.floor(Math.random() * secondaryStatsPossible.length)];
-
-                    secondaryStats[r] = this.getSecondaryStatValue(objectSubtype, r, level, ratio);
-
-                    // Not a bonus then
-                    if (Math.random() <= 0.2) {
-                        secondaryStats[r] = -secondaryStats[r];
-
-                        if (Math.random() <= 0.4 && secondaryStatsPossible.length - 1 > 0) {
-
-                            if (Math.random() <= 0.1 && secondaryStatsPossible.length - 2 > 0) {
-                                numberOfSecondaryStats++;
-                            }
-
-                            numberOfSecondaryStats++;
-                        }
-                    }
-
-                    secondaryStatsPossible.splice(statsPossible.indexOf(r), 1);
-                    numberOfSecondaryStats--;
-                }
+            if (equipable == true && Item.canHaveStats(objectType)) {
+                let newlyGeneratedStats = Item.generateItemsStats(rarity, objectType, objectSubtype, level, 100);
+                stats = newlyGeneratedStats.stats;
+                secondaryStats = newlyGeneratedStats.secondaryStats;
             }
 
 
             // Here stats have exactly the same architechture of a stats object
             // So we can calculate power
             let idInsert = await Item.lightInsert(idBase, level, Item.calculPower(stats, secondaryStats));
-            let statsValues = [];
-            let secondaryStatsValues = [];
-            let secondaryStatsElementalValues = [];
-
-            // Preparing Stats
-            for (let i in Globals.statsIdsByName) {
-                statsValues.push([idInsert, parseInt(Globals.statsIdsByName[i]), stats[i]]);
-            }
-
-            // Preparing Secondary Stats
-            for (let i in Globals.secondaryStatsIdsByName) {
-                secondaryStatsValues.push([idInsert, parseInt(Globals.secondaryStatsIdsByName[i]), secondaryStats[i]]);
-            }
-
-            // Preparing Elemental Resists
-            for (let i in Globals.elementsTypesIdsByName) {
-                secondaryStatsElementalValues.push([idInsert, parseInt(Globals.elementsTypesIdsByName[i]), secondaryStats[i + "Resist"]]);
-            }
-
-            await Promise.all([
-                conn.query("INSERT INTO itemsstats VALUES ?;", [statsValues]),
-                conn.query("INSERT INTO itemssecondarystats VALUES ?;", [secondaryStatsValues]),
-                secondaryStatsElementalValues.length > 0 ? conn.query("INSERT INTO itemssecondarystatselementalresists VALUES ?;", [secondaryStatsElementalValues]) : null,
-            ]);
+            await Item.replaceAllStats(idInsert, stats, secondaryStats);
 
             return idInsert;
 
         }
         return -1;
     }
+
 
     /**
      * 
@@ -367,52 +220,6 @@ class LootSystem {
             }
             default:
                 return true;
-        }
-    }
-
-    static canHaveStats(type) {
-        switch (type) {
-            case "resource":
-                return false;
-            case "mount":
-                return false;
-            default:
-                return true;
-        }
-    }
-
-    getStatsNumber(rarity) {
-        if (rarity < 6) {
-            return rarity - 1 + this.numberOfStatsBonus(rarity);
-        } else {
-            return Object.keys(Globals.statsIdsByName).length;
-        }
-    }
-
-    getSecondaryStatsNumber(rarity) {
-        return rarity + this.numberOfStatsBonus(rarity + 2);
-    }
-
-    numberOfStatsBonus(rarity) {
-        let maxPossible = 5 - rarity;
-        maxPossible = maxPossible < 0 ? 0 : maxPossible;
-        let maxStats = 0;
-        while (maxPossible > 0) {
-            if (Math.random() < 0.1) {
-                maxStats++;
-            }
-            maxPossible--;
-        }
-        return maxStats;
-    }
-
-    getRandomStatRatio(rarity, maxToPerfection = 100) {
-        if (rarity < 6) {
-            let min = (rarity - 1) / 5;
-            let max = rarity / 5;
-            return Math.random() * (max - min) + min;
-        } else {
-            return 1;
         }
     }
 
