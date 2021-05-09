c63f29d7042d400823fff95ce5a94f78b838d812
Roncarlos974 <9269672+Roncarlos@users.noreply.github.com>
2021-05-09 13:34:30 +0200

    added default lang

--- bin/Areas/AreaBonus.js
+++ bin/Areas/AreaBonus.js
@@ -32,11 +32,11 @@ class AreaBonus {
      * 
      * @param {string} lang 
      */
-    toStr(lang) {
+    toStr(lang="en") {
         return Translator.getString(lang, "bonuses", this.name) + " : " + this.getPercentage() + "%";
     }
 
-    toApi(lang) {
+    toApi(lang="en") {
         return {
             bonus_identifier: this.name,
             name: Translator.getString(lang, "bonuses", this.name),
