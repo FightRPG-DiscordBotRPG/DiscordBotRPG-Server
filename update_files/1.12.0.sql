61a4a9158d5d98ac9aef9819aaa386b59637dbef
Roncarlos974 <9269672+Roncarlos@users.noreply.github.com>
2021-05-15 14:13:55 +0200

    fix useless sql

--- update_files/1.12.0.sql
+++ update_files/1.12.0.sql
@@ -7,16 +7,6 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema discord_bot_rpg
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema discord_bot_rpg
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `discord_bot_rpg` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ;
-USE `discord_bot_rpg` ;
-
--- -----------------------------------------------------
 -- Table `discord_bot_rpg`.`EventsTypes`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `discord_bot_rpg`.`eventstypes` ;
