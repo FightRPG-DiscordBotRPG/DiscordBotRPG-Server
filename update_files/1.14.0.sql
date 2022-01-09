023cf825ed8e136cf76382e0a5884fe5b5dc6935
Roncarlos974 <9269672+Roncarlos@users.noreply.github.com>
2022-01-09 17:17:47 +0100

    players equipment sets (to db)

--- update_files/1.14.0.sql
+++ update_files/1.14.0.sql
@@ -60,3 +60,38 @@ CREATE TABLE IF NOT EXISTS `discord_bot_rpg`.`groupspendingplayers` (
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `discord_bot_rpg`.`playersequipmentsets` (
+  `idSet` BIGINT(19) UNSIGNED NOT NULL,
+  `name` VARCHAR(50) NOT NULL DEFAULT 'Nouveau Set',
+  `isFavorite` TINYINT(4) NOT NULL DEFAULT 0,
+  `creator` INT(10) UNSIGNED NOT NULL,
+  PRIMARY KEY (`idSet`),
+  INDEX `fk_PlayersEquipmentSets_Characters1_idx` (`creator` ASC) VISIBLE,
+  CONSTRAINT `fk_PlayersEquipmentSets_Characters1`
+    FOREIGN KEY (`creator`)
+    REFERENCES `discord_bot_rpg`.`Characters` (`idCharacter`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+CREATE TABLE IF NOT EXISTS `discord_bot_rpg`.`playersequipmentsetsitems` (
+  `idSet` BIGINT(19) UNSIGNED NOT NULL,
+  `idItem` BIGINT(19) UNSIGNED NOT NULL,
+  PRIMARY KEY (`idSet`, `idItem`),
+  INDEX `fk_PlayersEquipmentSetsItems_Items1_idx` (`idItem` ASC) VISIBLE,
+  CONSTRAINT `fk_PlayersEquipmentSetsItems_PlayersEquipmentSets1`
+    FOREIGN KEY (`idSet`)
+    REFERENCES `discord_bot_rpg`.`playersequipmentsets` (`idSet`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_PlayersEquipmentSetsItems_Items1`
+    FOREIGN KEY (`idItem`)
+    REFERENCES `discord_bot_rpg`.`items` (`idItem`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
